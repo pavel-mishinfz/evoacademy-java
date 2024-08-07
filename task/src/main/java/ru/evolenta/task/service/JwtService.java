0791e2e32a1680b80a373eb70c6f1ee5eaea254f
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-08-07 23:30:18 +0300

    Add jwt processing logic to task service

---
+++ task/src/main/java/ru/evolenta/task/service/JwtService.java
@@ -0,0 +1,95 @@
+package ru.evolenta.task.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    /**
+     * Извлечение имени пользователя из токена
+     *
+     * @param token токен
+     * @return имя пользователя
+     */
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    /**
+     * Извлечение ролей пользователя из токена
+     *
+     * @param token токен
+     * @return список ролей пользователя
+     */
+    public String extractUserRole(String token) {
+        final Claims claims = extractAllClaims(token);
+        return (String) claims.get("role");
+    }
+
+    /**
+     * Проверка токена на валидность
+     *
+     * @param token       токен
+     * @return true, если токен валиден
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Извлечение данных из токена
+     *
+     * @param token           токен
+     * @param claimsResolvers функция извлечения данных
+     * @param <T>             тип данных
+     * @return данные
+     */
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    /**
+     * Извлечение всех данных из токена
+     *
+     * @param token токен
+     * @return данные
+     */
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+    * Получение ключа для подписи токена
+    *
+    * @return ключ
+    * */
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
